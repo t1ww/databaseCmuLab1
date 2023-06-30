da6cc66e724ff61fd549f66d6d63de0a6b864d87
t1ww <95153968+t1ww@users.noreply.github.com>
2023-06-30 19:15:15 +0700

    added file writing and fixed ans sheet

--- lab1/databaseLab1.java
+++ lab1/databaseLab1.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class databaseLab1 {
     public static void main(String[] args) {
         try {
-            ArrayList uniList = new ArrayList<objUniversity>();
+            ArrayList<objUniversity> uniList = new ArrayList<objUniversity>();
             Scanner scan = new Scanner(new File("lab1/QS World University Rankings 2017.csv"));
             scan.nextLine();
             while(scan.hasNextLine()){
@@ -37,9 +38,21 @@ public class databaseLab1 {
             ,"Chiang Mai"
             ,"North thailand"
             ,"https://en.wikipedia.org/wiki/Chiang_Mai_University"));
+
+            // writing into csv
+
+            PrintWriter writer = new PrintWriter("lab1/QS World University Rankings 2017_out.csv");
+            writer.println("Year,Rank,University,Score,Link,Country,City,Region,Logo");
             for (int index = 0; index < uniList.size(); index++) {
                 ((objUniversity) uniList.get(index)).ptoString();
+                writer.println(uniList.get(index).getYear() + "," + uniList.get(index).getRank_display() + "," + uniList.get(index).getUniversity() + "," +
+                uniList.get(index).getScore() + "," + uniList.get(index).getLink() + "," + uniList.get(index).getCountry() + "," +
+                uniList.get(index).getCity() + "," + uniList.get(index).getRegion() + "," + uniList.get(index).getLogo());
             }
+            writer.close();
+            scan.close();
+            /// program end
+            System.out.println("program terminated successfully");
         } catch (FileNotFoundException err) {
             System.out.println("Error can't find file" + err);
         }
